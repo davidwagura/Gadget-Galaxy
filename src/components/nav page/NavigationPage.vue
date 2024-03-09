79bb8de68032e64b08df2e6c8862d2784044d736
David wanjohi <davidwagura83@gmail.com>
2024-03-09 16:04:19 +0300

    add Shop buttons

--- src/components/nav page/NavigationPage.vue
+++ src/components/nav page/NavigationPage.vue
@@ -48,57 +48,50 @@
         </div>
     </div>
     <div class="mt-16">
-        <div class="border bg-blue-500 text-xl p-8 text-left m-8">
+        <div class="border text-xl p-8 text-left m-8"
+        :style="{'background-image': 'url(/src/assets/elegant-smartphone-composition_23-2149437106.webp)'}">
             <h2 class="font-bold">Smartphones</h2><br>
-            <p>Stay connected in style with our sleek and powerful smartphones. From stunning displays to lightning-fast processors, our devices redefine the mobile experience.</p>
-            <button class="bg-red-500">
-                <router-link to="/smartphones">
-                    <span>Shop Now
-                        <svg class="h-5 w-5 mx-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                            <path d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
-                        </svg>
-                    </span>
-                </router-link>
-            </button>
+            <p class="mb-6">Stay connected in style with our sleek and powerful smartphones. From stunning displays to lightning-fast processors, our devices redefine the mobile experience.</p>
+            <a class="bg-gray-500 border rounded w-1/4 mt-3 p-4" href="/smartphones">Shop Now</a>
         </div>
         <div class="flex flex-col">
-            <div class="flex flex-col border border-black text-xl p-8 m-8">
-                <h2 class="font-bold">Computers and Laptops</h2>
-                <p>Unleash your productivity with our range of cutting-edge laptops and computers.</p>
-                <p>Whether you're a student, professional, or creative, find the perfect balance of performance and portability.</p>
-            </div>
-            <div class="border border-black flex flex-col text-xl p-8 m-8">
-                <h1 class="font-bold">Gaming Console</h1>
-                <p>Embark on epic adventures and multiplayer showdowns with our cutting-edge gaming consoles.</p>
-                <p>From high-octane action to immersive storytelling, experience gaming like never before.</p>
+            <div class="inline-flex">
+                <div class="flex flex-col  border border-black text-xl p-8 m-8">
+                    <h2 class="font-bold mb-4">Laptops and Computers</h2>
+                    <p>Empower your productivity and unleash your creativity with our cutting-edge laptops and computers. From sleek ultrabooks to powerhouse gaming rigs, our collection offers the perfect blend of performance and portability.</p>
+                    <p>Whether you're crunching numbers, designing masterpieces, or diving into immersive gaming worlds, our laptops and computers are your gateway to limitless possibilities.</p>
+                    <a class="bg-gray-500 border rounded w-1/4 mt-3 p-4" href="/laptops-phones">Shop Now</a>
+                </div>
+                <div class="flex flex-col border border-black text-xl p-8 m-8">
+                    <h1 class="font-bold mb-4">Gaming Console</h1>
+                    <p>Experience gaming nirvana with our selection of gaming consoles. From epic adventures to thrilling multiplayer battles, immerse yourself in a world of entertainment like never before.</p>
+                    <p>Choose from the latest consoles and accessories to elevate your gaming experience and become the ultimate champion of virtual realms.</p>
+                    <a class="bg-gray-500 border rounded w-32 mt-3 p-4" href="/gaming-console">Shop Now</a>
+                </div>
             </div>
         </div>
         <div class="flex flex-col">
-            <div class="flex flex-col border border-black text-xl p-8 m-8">
-                <h2 class="font-bold">Accesories</h2>
-                <p>Complete your tech arsenal with our curated collection of accessories. From sleek phone cases to powerful chargers, find the perfect complement to enhance and protect your devices.</p>
-                <p>Elevate your style and functionality with our range of must-have accessories.</p>
+            <div class="inline-flex">
+                <div class="flex flex-col border border-black text-xl p-8 m-8">
+                    <h2 class="font-bold mb-4">Accessories</h2>
+                    <p>Complete your tech arsenal with our curated collection of accessories. From sleek phone cases to powerful chargers, find the perfect complement to enhance and protect your devices.</p>
+                    <p>Elevate your style and functionality with our range of must-have accessories.</p>
+                    <a class="bg-gray-500 border rounded w-1/4 mt-3 p-4" href="/accessories">Shop Now</a>
+                </div>
+                <div class="flex flex-col border border-black text-xl p-8 m-8 ml-auto">
+                    <h1 class="font-bold mb-4">Audio and Headphones</h1>
+                    <p>Dive into a world of sound with our premium audio and headphones collection. Immerse yourself in crystal-clear audio, whether you're enjoying your favorite music, gaming, or taking calls. </p>
+                    <p>From wireless earbuds to over-ear headphones, experience every beat and note with unparalleled clarity and comfort.</p>
+                    <a class="bg-gray-500 border rounded w-1/4 mt-3 p-4" href="/audio-headphones">Shop Now</a>
 
-            </div>
-            <div class="border border-black flex flex-col text-xl p-8 m-8">
-                <h1 class="font-bold">Audio and Headphones</h1>
-                <p>Dive into a world of sound with our premium audio and headphones collection. Immerse yourself in crystal-clear audio, whether you're enjoying your favorite music, gaming, or taking calls. </p>
-                <p>From wireless earbuds to over-ear headphones, experience every beat and note with unparalleled clarity and comfort.</p>
+                </div>
             </div>
         </div>
         <div class="border bg-blue-500 text-xl p-8 text-left m-8">
             <h2 class="font-bold">Cameras</h2><br>
             <p>Capture life's moments in stunning detail with our diverse range of cameras.</p>
-            <p> From compact point-and-shoots to professional-grade DSLRs, our collection offers something for every photographer. Unleash your creativity, preserve memories, and document adventures with our high-quality cameras designed to elevate your photography game.</p>
-            <button class="bg-red-500">
-                <router-link to="/smartphones">
-                    <span>Shop Now
-                        <svg class="h-5 w-5 mx-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                            <path d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
-                        </svg>
-                    </span>
-                </router-link>
-            </button>
+            <p class="mb-8"> From compact point-and-shoots to professional-grade DSLRs, our collection offers something for every photographer. Unleash your creativity, preserve memories, and document adventures with our high-quality cameras designed to elevate your photography game.</p>
+            <a class="bg-gray-500 border rounded w-1/4  p-4" href="/cameras">Shop Now</a>
         </div>
 
     </div>
