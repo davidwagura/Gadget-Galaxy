7d7151de6819a8bab0dca79ddc4e9fc6bf767af6
David wanjohi <davidwagura83@gmail.com>
2024-03-17 17:16:19 +0300

    add script

--- src/components/profile/LoginPage.vue
+++ src/components/profile/LoginPage.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex justify-center items-center h-screen">
-        <form class="bg-white shadow-md rounded border px-8 pt-6 pb-8 mb-4 w-1/3">
+        <form @submit.prevent ="loginForm" class="bg-white shadow-md rounded border px-8 pt-6 pb-8 mb-4 w-1/3">
             <div class="mb-4">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
                     Email
@@ -13,6 +13,9 @@
                 </label>
                 <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
             </div>
+            <div class="font-medium mb-4">
+                <p>Don't have an account? <a href="http://localhost:8080/register" class="text-blue-500">Register</a></p>
+            </div>
             <div class="flex items-center justify-between">
                 <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
                     Login
@@ -21,3 +24,25 @@
         </form>
     </div>
 </template>
+
+<script>
+import axios from 'axios' 
+
+export default {
+    name: 'LoginPage',
+
+    data() {
+        return {
+            email: '',
+            password: '',
+        };
+    },
+
+    methods: {
+        loginForm() {
+            axios.get('http://127.0.0.1:8000/api/student', this.data)
+
+        }
+    }
+}
+</script>
--- src/components/profile/RegisterPage.vue
+++ src/components/profile/RegisterPage.vue
@@ -1,23 +1,23 @@
 <template>
     <div class="flex justify-center items-center h-screen">
-        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-1/3">
+        <form @submit.prevent ="submitForm" class="bg-white shadow-md border rounded px-8 pt-6 pb-8 mb-4 w-1/3">
             <div class="mb-4">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
                     Username
                 </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" v-model="username" type="text" placeholder="Username">
             </div>
             <div class="mb-4">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
                     Email
                 </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email">
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" v-model="email" placeholder="Email">
             </div>
             <div class="mb-6">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
                     Password
                 </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" v-model="password" placeholder="******************">
             </div>
             <div class="flex items-center justify-between">
                 <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
@@ -27,3 +27,34 @@
         </form>
     </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'RegisterPage',
+
+    data() {
+        return {
+            username: '',
+            email: '',
+            password: '',
+        };
+    },
+
+    methods: {
+        submitForm() {
+            axios.post('http://127.0.0.1:8000/api/student', this.data)
+                .then(response => {
+                    console.log(response.data);
+
+                    // username: this.username
+                    // email: this.email
+                    // password: this.password
+                });
+                this.$router.push('/login')
+        }
+
+    }
+}
+</script>
