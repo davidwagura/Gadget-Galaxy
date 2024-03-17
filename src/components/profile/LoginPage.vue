c8167a590993452116a07a407b06bc876237644b
David wanjohi <davidwagura83@gmail.com>
2024-03-17 12:17:40 +0300

    add login page

--- src/components/profile/LoginPage.vue
+++ src/components/profile/LoginPage.vue
@@ -1,5 +1,23 @@
 <template>
-    <div>
-        <h1>login</h1>
+    <div class="flex justify-center items-center h-screen">
+        <form class="bg-white shadow-md rounded border px-8 pt-6 pb-8 mb-4 w-1/3">
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+                    Email
+                </label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email">
+            </div>
+            <div class="mb-6">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                    Password
+                </label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+            </div>
+            <div class="flex items-center justify-between">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                    Login
+                </button>
+            </div>
+        </form>
     </div>
 </template>
