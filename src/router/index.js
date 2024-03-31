d013e93868f6dc717a2e850bf79b30a5ee5c3aed
David wanjohi <davidwagura83@gmail.com>
2024-03-31 15:46:50 +0300

    Create the about page

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import NavigationPage from '../components/nav page/NavigationPage.vue'
 import LoginPage from '../components/profile/LoginPage.vue'
 import RegisterPage from '../components/profile/RegisterPage.vue'
+import AboutPage from '@/components/AboutPage.vue'
 
 
 const routes = [
     { path: '/', component: NavigationPage },
     { path: '/login', component: LoginPage },
-    { path: '/register', component: RegisterPage }
+    { path: '/register', component: RegisterPage },
+    { path: '/about', component: AboutPage },
 ]
 
 const router = createRouter({
