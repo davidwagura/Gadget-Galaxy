8d5f59f2c93fd600645a49b39b85454e850f3de5
David wanjohi <davidwagura83@gmail.com>
2024-05-26 19:13:38 +0300

    Remove border

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import NavigationPage from '../components/nav page/NavigationPage.vue'
+
 import LoginPage from '../components/profile/LoginPage.vue'
+
 import RegisterPage from '../components/profile/RegisterPage.vue'
+
 import AboutPage from '@/components/AboutPage.vue'
+
 import ContactPage from '@/components/ContactPage.vue'
 
 
 const routes = [
+
     { path: '/', component: NavigationPage },
+
     { path: '/login', component: LoginPage },
+
     { path: '/register', component: RegisterPage },
+
     { path: '/about', component: AboutPage },
+
     { path: '/contact', component: ContactPage },
+
 ]
 
 const router = createRouter({
+
     history: createWebHistory(),
+
     routes
+    
 })
 
 export default router
