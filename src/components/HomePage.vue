7bb5350482e0000d057c6907735e4357839d98cd
David wanjohi <davidwagura83@gmail.com>
2025-02-11 21:50:25 +0300

    Create homepage file

---
+++ src/components/HomePage.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="mt-16 text-white">
+
+<div class="rounded-lg p-8 text-left m-8 bg-cover bg-center" id="phone">
+
+    <h2 class="font-bold">Smartphones</h2><br>
+
+    <p class="mb-6">Stay connected in style with our sleek and powerful smartphones. From stunning displays to lightning-fast processors, our devices redefine the mobile experience.</p>
+
+    <a class="bg-blue-500 border rounded w-1/4 mt-3 p-4" href="/smartphones">Shop Now</a>
+
+</div>
+
+<div class="flex flex-col">
+
+    <div class="inline-flex">
+
+        <div class="flex flex-col p-8 m-8 rounded-lg" id="laptop">
+
+            <h2 class="font-bold mb-4">Laptops and Computers</h2>
+
+            <p>Empower your productivity and unleash your creativity with our cutting-edge laptops and computers. From sleek ultrabooks to powerhouse gaming rigs, our collection offers the perfect blend of performance and portability.</p>
+
+            <p>Whether you're crunching numbers, designing masterpieces, or diving into immersive gaming worlds, our laptops and computers are your gateway to limitless possibilities.</p>
+
+            <a class="bg-blue-500 border rounded w-1/4 mt-3 p-4" href="/laptops-phones">Shop Now</a>
+
+        </div>
+
+        <div class="flex rounded-lg flex-col p-8 m-8" id="games">
+
+            <h1 class="font-bold mb-4">Gaming Console</h1>
+
+            <p>Experience gaming nirvana with our selection of gaming consoles. From epic adventures to thrilling multiplayer battles, immerse yourself in a world of entertainment like never before.</p>
+
+            <p>Choose from the latest consoles and accessories to elevate your gaming experience and become the ultimate champion of virtual realms.</p>
+
+            <a class="bg-blue-500 border rounded w-32 mt-3 p-4" href="/gaming-console">Shop Now</a>
+
+        </div>
+
+    </div>
+
+</div>
+
+<div class="flex flex-col">
+
+    <div class="inline-flex">
+
+
+        <div class="flex flex-col rounded-lg p-8 m-8" id="accessories">
+
+            <h2 class="font-bold mb-4">Accessories</h2>
+
+            <p>Complete your tech arsenal with our curated collection of accessories. From sleek phone cases to powerful chargers, find the perfect complement to enhance and protect your devices.</p>
+
+            <p>Elevate your style and functionality with our range of must-have accessories.</p>
+
+            <a class="bg-blue-500 border rounded w-1/4 mt-3 p-4" href="/accessories">Shop Now</a>
+
+        </div>
+
+        <div class="flex flex-col rounded-lg p-8 m-8 ml-auto" id="audio">
+
+            <h1 class="font-bold mb-4">Audio and Headphones</h1>
+
+            <p>Dive into a world of sound with our premium audio and headphones collection. Immerse yourself in crystal-clear audio, whether you're enjoying your favorite music, gaming, or taking calls. </p>
+
+            <p>From wireless earbuds to over-ear headphones, experience every beat and note with unparalleled clarity and comfort.</p>
+
+            <a class="bg-blue-500 border rounded w-1/4 mt-3 p-4" href="/audio-headphones">Shop Now</a>
+
+        </div>
+
+    </div>
+
+</div>
+
+<div class="p-8 text-left rounded-lg m-8" id="camera">
+
+    <h2 class="font-bold">Cameras</h2><br>
+
+    <p>Capture life's moments in stunning detail with our diverse range of cameras.</p>
+
+    <p class="mb-8"> From compact point-and-shoots to professional-grade DSLRs, our collection offers something for every photographer. Unleash your creativity, preserve memories, and document adventures with our high-quality cameras designed to elevate your photography game.</p>
+
+    <a class="bg-blue-500 border rounded w-1/4  p-4" href="/cameras">Shop Now</a>
+
+</div>
+
+</div>
+
+</template>
+
+<script>
+</script>
