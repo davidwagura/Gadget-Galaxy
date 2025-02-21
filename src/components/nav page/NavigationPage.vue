17a84e1ae5ad77095cc79ebb7af4acf28fafd9ab
David wanjohi <davidwagura83@gmail.com>
2025-02-21 21:40:01 +0300

    Clean code

--- src/components/nav page/NavigationPage.vue
+++ src/components/nav page/NavigationPage.vue
@@ -151,7 +151,8 @@
     background-repeat: no-repeat;
 }
 #audio {
-    background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/src/assets/headphones1');
+    /* background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/src/assets/headphones'); */
+    background-color: gray;
 
 }
 </style>
